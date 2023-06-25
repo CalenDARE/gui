2cfe594734114167b5a5f5cf79457a35c1674df0
SaricArmin <armin.saric@stud.fh-campuswien.ac.at>
2023-06-25 22:45:34 +0200

    updated gui

--- server/files/leagues.js
+++ server/files/leagues.js
@@ -1,74 +1,75 @@
 function setMovie(movie) {
 
-  for (const element of document.forms[0].elements) {
-    const name = element.id;
-    const value = movie[name];
-    
-    if (name === "Genres") {
-      const options = element.options;
-      for (let index = 0; index < options.length; index++) {
-        const option = options[index];
-        option.selected = value.indexOf(option.value) >= 0;
-      }
-    } else {
-      element.value = value;
-    }
-  }
-}
-
-function getMovie() {
-  const movie = {};
-
-  const elements = Array.from(document.forms[0].elements).filter(element => element.id)
-
-  for (const element of elements) {
-    const name = element.id;
-
-    let value;
-
-    if (name === "Genres") {
-      value = [];
-      const options = element.options;
-      for (let index = 0; index < options.length; index++) {
-        const option = options[index];
-        if (option.selected) {
-          value.push(option.value);
+    for (const element of document.forms[0].elements) {
+      const name = element.id;
+      const value = movie[name];
+      
+      if (name === "Genres") {
+        const options = element.options;
+        for (let index = 0; index < options.length; index++) {
+          const option = options[index];
+          option.selected = value.indexOf(option.value) >= 0;
         }
+      } else {
+        element.value = value;
       }
-    } else if (name === "Metascore" || name === "Runtime" || name === "imdbRating") {
-        value = Number(element.value);
-    } else if (name === "Actors" || name === "Directors" || name === "Writers") {
-      value = element.value.split(",").map((item) => item.trim());
-    } else {
-      value = element.value;
-    }
-
-    movie[name] = value;
-  }
-
-  return movie;
-}
-
-function putMovie() {
-  const movie = getMovie();
-
-  const xhr = new XMLHttpRequest()
-  xhr.onload = function() {
-    if (xhr.status == 200 || xhr.status === 204) {
-      location.href = 'index.html'
-    } else {
-      alert("Saving of movie data failed. Status code was " + response.status)
     }
   }
   
-  xhr.open("PUT", "/movies/" + movie.imdbID)
-  xhr.setRequestHeader("Content-Type", "application/json")
-
-  xhr.send(JSON.stringify(movie))
-
-}
-
-function loadMovies(genre) {
-  console.log("test");
-}
-
+  function getMovie() {
+    const movie = {};
+  
+    const elements = Array.from(document.forms[0].elements).filter(element => element.id)
+  
+    for (const element of elements) {
+      const name = element.id;
+  
+      let value;
+  
+      if (name === "Genres") {
+        value = [];
+        const options = element.options;
+        for (let index = 0; index < options.length; index++) {
+          const option = options[index];
+          if (option.selected) {
+            value.push(option.value);
+          }
+        }
+      } else if (name === "Metascore" || name === "Runtime" || name === "imdbRating") {
+          value = Number(element.value);
+      } else if (name === "Actors" || name === "Directors" || name === "Writers") {
+        value = element.value.split(",").map((item) => item.trim());
+      } else {
+        value = element.value;
+      }
+  
+      movie[name] = value;
+    }
+  
+    return movie;
+  }
+  
+  function putMovie() {
+    const movie = getMovie();
+  
+    const xhr = new XMLHttpRequest()
+    xhr.onload = function() {
+      if (xhr.status == 200 || xhr.status === 204) {
+        location.href = 'index.html'
+      } else {
+        alert("Saving of movie data failed. Status code was " + response.status)
+      }
+    }
+    
+    xhr.open("PUT", "/movies/" + movie.imdbID)
+    xhr.setRequestHeader("Content-Type", "application/json")
+  
+    xhr.send(JSON.stringify(movie))
+  
+  }
+  
+  function loadMovies(genre) {
+    console.log("test");
+  }
+  
+  
