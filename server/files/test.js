f446e0dc81602782cc900d672cfc6766a26919e1
SaricArmin <armin.saric@stud.fh-campuswien.ac.at>
2023-06-27 17:44:36 +0200

    UPDATE

--- server/files/test.js
+++ server/files/test.js
@@ -62,56 +62,61 @@ function getData(source) {
 }
 
 function appendMatches(data) {
-    const bodyElement = document.querySelector("body")
-    const matches = document.createElement("table")
-    matches.id = "matches"
-    matches.classList.add("centered-container")
+    const bodyElement = document.querySelector("body");
+    const matches = document.createElement("table");
+    matches.id = "matches";
+    matches.classList.add("centered-container");
   
     for (const match of data) {
-        const tableRow = document.createElement("tr")
-        const addToFavoritesButton = document.createElement("button")
+        const tableRow = document.createElement("tr");
+        const addToFavoritesButton = document.createElement("button");
 
-        const homeLogoCol = document.createElement("td")
-        const awayLogoCol = document.createElement("td")
-        const homeTeamCol = document.createElement("td")
-        const awayTeamCol = document.createElement("td")
-        const vsListCol = document.createElement("td")
-        const dateListCol = document.createElement("td")
+        const homeLogoCol = document.createElement("td");
+        const awayLogoCol = document.createElement("td");
+        const homeTeamCol = document.createElement("td");
+        const awayTeamCol = document.createElement("td");
+        const vsListCol = document.createElement("td");
+        const dateListCol = document.createElement("td");
 
-        dateListCol.textContent = match.date
-        dateListCol.style.paddingLeft = '400px'
-        dateListCol.style.paddingRight = '100px'
-        tableRow.appendChild(dateListCol)
+        dateListCol.textContent = match.date;
+        dateListCol.style.paddingLeft = '400px';
+        dateListCol.style.paddingRight = '100px';
+        tableRow.appendChild(dateListCol);
 
-        const homeLogo = document.createElement("img")
-        homeLogo.src = match.teams.home.logo
-        homeLogo.width = 70
-        homeLogo.height = 70
-        homeLogoCol.appendChild(homeLogo)
-        tableRow.appendChild(homeLogoCol)
+        const homeLogo = document.createElement("img");
+        homeLogo.src = match.teams.home.logo;
+        homeLogo.width = 70;
+        homeLogo.height = 70;
+        homeLogoCol.appendChild(homeLogo);
+        tableRow.appendChild(homeLogoCol);
 
-        homeTeamCol.textContent = match.teams.home.name
-        tableRow.appendChild(homeTeamCol)
+        homeTeamCol.textContent = match.teams.home.name;
+        tableRow.appendChild(homeTeamCol);
 
-        vsListCol.textContent = "VS"
-        tableRow.appendChild(vsListCol)
+        vsListCol.textContent = "VS";
+        tableRow.appendChild(vsListCol);
 
-        awayTeamCol.textContent = match.teams.away.name
-        awayTeamCol.style.textAlign = "right"
-        tableRow.appendChild(awayTeamCol)
+        awayTeamCol.textContent = match.teams.away.name;
+        awayTeamCol.style.textAlign = "right";
+        tableRow.appendChild(awayTeamCol);
 
-        const awayLogo = document.createElement("img")
-        awayLogo.src = match.teams.away.logo
-        awayLogo.width = 70
-        awayLogo.height = 70
-        awayLogoCol.appendChild(awayLogo)
-        tableRow.appendChild(awayLogoCol)
+        const awayLogo = document.createElement("img");
+        awayLogo.src = match.teams.away.logo;
+        awayLogo.width = 70;
+        awayLogo.height = 70;
+        awayLogoCol.appendChild(awayLogo);
+        awayLogoCol.style.paddingRight = '250px';
+        tableRow.appendChild(awayLogoCol);
 
-
-        addToFavoritesButton.textContent = "❤"
-        tableRow.appendChild(addToFavoritesButton)
-        matches.appendChild(tableRow)
+        addToFavoritesButton.textContent = "❤";
+        addToFavoritesButton.style.fontSize = "40px";
+        addToFavoritesButton.style.padding = '5px';
+        addToFavoritesButton.style.height = '60px';
+        addToFavoritesButton.style.width = '60px';
+        addToFavoritesButton.style.marginTop = '22px'
+        tableRow.appendChild(addToFavoritesButton);
+        matches.appendChild(tableRow);
     }
   
-    bodyElement.appendChild(matches)
+    bodyElement.appendChild(matches);
 }
